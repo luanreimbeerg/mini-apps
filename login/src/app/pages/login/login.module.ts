0c8f375e7ae2b6e18ee5aa77f670b7558eca22e6
Luan Trajano <luan.trajano@biz.com.br>
2022-07-26 20:31:10 -0300

    ajuste

--- login/src/app/pages/login/login.module.ts
+++ login/src/app/pages/login/login.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from "./login-routing.module";
+import { LoginComponent } from "./login.component";
 
-import { MatInputModule } from '@angular/material/input';
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, MatInputModule],
+  imports: [CommonModule, LoginRoutingModule, MatInputModule, MatIconModule],
 })
 export class LoginModule {}
